cf10fd6cb6738d1e41bb31dba328794ad499c64a
newmarwegner <newmarwegner@gmail.com>
2021-07-30 16:34:34 -0300

    merge function

--- agriculture/sentineldownloader.py
+++ agriculture/sentineldownloader.py
@@ -1,12 +1,14 @@
 import os
 import shutil
-from datetime import date
-
 import ee
 import geopandas as gpd
 import geemap
+import rasterio
+from rasterio.merge import merge
+from itertools import groupby
 from decouple import config
 from shapely.geometry import LineString, MultiLineString
+from datetime import date
 
 
 class SentinelHandler:
@@ -62,7 +64,9 @@ class SentinelHandler:
         :return: geodataframe divided
         """
         ds_in = geodataframe
-        num_tiles = int(ds_in['geometry'].to_crs(epsg=5880).area[0] / 220300)
+        # divide = int(ds_in['geometry'].to_crs(epsg=5880).area[0] / 220300)
+        num_tiles = int(ds_in['geometry'].to_crs(epsg=5880).area[0] / 500000)
+        print(num_tiles)
         minx, miny, maxx, maxy = [i[1][0] for i in ds_in.bounds.items()]
         dx = (maxx - minx) / num_tiles
         dy = (maxy - miny) / num_tiles
@@ -120,6 +124,42 @@ class SentinelHandler:
         box = [[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin], [xmin, ymin]]
         
         return filter_polygon, box
+    
+    def rename_path_rasters(self):
+        """
+        Method to rename rasters according to date
+        """
+        output_paths = []
+        for root, directory, files in os.walk('../output'):
+            for file in files:
+                if file.endswith('.tif'):
+                    dir_path = root + '/'
+                    os.rename(dir_path + file, dir_path + file[0:8] + '.tif')
+                    full_path = f"{dir_path}{dir_path.split('/')[-2]}_{file.replace(file, file[0:8] + '.tif')}"
+                    output_paths.append(full_path)
+    
+    def down_paths(self):
+        """
+        Method to create a list of download images paths to be used in create a mosaic
+        :return: list of paths
+        """
+        self.rename_path_rasters()
+        paths = []
+        for root, directory, files in os.walk(self.get_abs_path('output')):
+            if not root.endswith('output'):
+                for file in files:
+                    paths.append(os.path.join(root, file))
+        
+        return paths
+        
+    def grouppath_mosaic(self, paths_to_mosaic):
+        """
+            Method to grouppaths to create a input to mosaic
+            """
+        func = lambda x: x[-11:]
+        temp = sorted(paths_to_mosaic, key=func)
+       
+        return [list(paths_to_mosaic) for i, paths_to_mosaic in groupby(temp, func)]
 
 
 class SentinelDownloader:
@@ -152,6 +192,35 @@ class SentinelDownloader:
         
         geemap.ee_export_image_collection(collection, scale=10, crs='EPSG:4326', region=boundary, out_dir=out_dir)
     
+    def create_mosaic(self):
+        """
+        Method to create mosaic with all download images
+        """
+        self.handler.create_folder('../merged')
+        src_files_to_mosaic = []
+        out_meta = []
+        for i in self.handler.grouppath_mosaic(self.handler.down_paths()):
+            ano = i[0].split('/')[-1][-12:-4]
+            
+            for file in i:
+                full = ''.join([file[:file.rfind('/') + 1], ano + '.tif'])
+                src = rasterio.open(full)
+                src_files_to_mosaic.append(src)
+                out_meta = src.meta.copy()
+        
+            mosaic, out_trans = merge(src_files_to_mosaic)
+            out_meta.update({"driver": "GTiff",
+                             "height": mosaic.shape[1],
+                             "width": mosaic.shape[2],
+                             "transform": out_trans,
+                             "crs": "+proj=longlat +datum=WGS84 +no_defs"
+                             }
+                            )
+            with rasterio.open(f'../merged/{ano}.tif', "w", **out_meta) as dest:
+                dest.write(mosaic)
+        
+        self.handler.del_folders(['output',])
+        
     def download_sentinel(self, vector_limit, driver, filter_field, start_date):
         """
         Method to download sentinel images from gee
@@ -166,9 +235,12 @@ class SentinelDownloader:
         for feature_name in feature_names:
             filter_polygon, box = self.handler.get_polygon_filtered(filter_field, feature_name, gdf)
             self.down_geemap(feature_name, box, start_date)
-
+        
+        self.create_mosaic()
 
 if __name__ == '__main__':
     sd = SentinelDownloader()
-    sd.authenticate_gee()
-    sd.download_sentinel('vector_tasca_test.gpkg', 'gpkg', 'id_pk', '2021-07-01')
+    # sd.authenticate_gee()
+    # sd.download_sentinel('teste.gpkg', 'gpkg', 'id_pk', '2021-07-15')
+
+
